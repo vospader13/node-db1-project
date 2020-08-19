52381280a35526c6c5645b36a82983071c12fd26
vospader13 <justenmcdonald@gmail.com>
2020-08-19 17:45:28 -0600

    completed assignment

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ server.use("/accounts", accountRouter)
 server.use((err, req, res, next) => {
     console.log(err)
     res.status(500).json({
-        message: "Something went wrong",
+        message: "Something went wrong.",
     })
 })
 
